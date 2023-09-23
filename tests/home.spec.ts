b4fd9a0bb48065efb69c10aeb91d376a7a76c995
lucas-scandido <luccas.candido@outlook.com>
2023-09-23 11:10:33 -0300

    Test adjustment

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test'
 test.describe('Application Status', () => {
 
   test('App should be online', async ({ page }) => {
-    await page.goto('/')
-  
+    await page.goto('/', { waitUntil: "networkidle" })
+
     await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
   })
 })
